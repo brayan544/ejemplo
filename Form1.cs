b6314560a9049dd5af00503ed684d6b4f1ae7182
brayanguilli96 <brayanguilli96@gmail.com>
2019-01-08 08:04:40 -0500

    boton guardar agregado

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,11 @@ namespace github
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Mensaje gift");
+
+        }
     }
 }
